bdc57204c6e6a221f68ef81cfd71fa92eb107703
Walker Neudorff <walkerneudorff@gmail.com>
2023-11-24 17:49:52 -0600

    Made seperate route and router file for upload POST

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,32 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js');
-const multer  = require('multer')
-const cloudinary = require("../utils/cloudinary");
-const upload = multer({ dest: './public/data/uploads/' })
-
-router.post('/upload', upload.single('uploaded_file'), async (req, res) => {
-  // req.file is the name of your file in the form, here 'uploaded_file'
-  // req.body will hold the text fields, if there were any
-  console.log('req, req.file, req.body', req, req.file, req.body)
-    // Upload image to cloudinary
-  const result = await cloudinary.uploader.upload(req.file.path, { public_id: req.body.name } )
-  console.log('result', result)
-  const sqlText = `
-      INSERT INTO "gallery" 
-          ("url", "title", "description")
-        VALUES 
-          ($1, $2, $3)`;
-  pool.query(sqlText, [result.secure_url, req.body.name, req.body.description])
-      .then((result) => {
-        console.log(`Added image to the database`);
-        res.sendStatus(201);
-      })
-      .catch((dbError) => {
-        console.log("upload image failed", dbError);
-        res.sendStatus(500);
-    })
-});
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,11 +1,35 @@
 import axios from 'axios'
+import { useState } from 'react'
 
 function UploadForm () {
 
+    let [titleInput, setTitleInput] = useState('')
+    let [descriptionInput, setDescriptionInput] = useState('')
+
+    const uploadImg = () => {    
+        axios.post('/gallery/upload', { 
+            title: titleInput, 
+            description: descriptionInput
+        })
+          .then(response => {
+            setTitleInput('');
+            setDescriptionInput('');
+            getImgs();
+          })
+          .catch(err => {
+            alert('Error Adding shopping items');
+            console.log(err);
+          })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        uploadImg();
+    }
     return (
         <>
         <form 
-            action="/gallery/upload" 
+            action="/upload" 
             encType="multipart/form-data" 
             method="post">
             <div className="form-group">
@@ -17,12 +41,12 @@ function UploadForm () {
             <input 
                 type="text" 
                 className="form-control" 
-                placeholder="Image Name" 
-                name="name" />
+                placeholder="Image Title" 
+                name="name"/>
             <input 
                 type="text" 
                 placeholder="Image Description" 
-                name="description" />
+                name="description"/>
             <input 
                 type="submit" 
                 value="Submit Photo" 
